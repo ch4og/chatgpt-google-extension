c725f55d1b224e0a740f5d4b4b57dfb38f06573d
Nikita Mitasov <32384814+ch4og@users.noreply.github.com>
2023-03-05 21:50:26 +0300

    Update openai.ts

--- src/background/providers/openai.ts
+++ src/background/providers/openai.ts
@@ -13,10 +13,12 @@ export class OpenAIProvider implements Provider {
     }
     return prompt
   }
-
+  private buildMessages(prompt: string) {
+    return [{ role: 'user', content: prompt }]
+  }
   async generateAnswer(params: GenerateAnswerParams) {
     let result = ''
-    await fetchSSE('https://api.openai.com/v1/completions', {
+    await fetchSSE('https://api.openai.com/v1/chat/completions', {
       method: 'POST',
       signal: params.signal,
       headers: {
@@ -25,7 +27,7 @@ export class OpenAIProvider implements Provider {
       },
       body: JSON.stringify({
         model: this.model,
-        prompt: this.buildPrompt(params.prompt),
+        messages: this.buildMessages(params.prompt),
         stream: true,
         max_tokens: 2048,
       }),
@@ -38,8 +40,8 @@ export class OpenAIProvider implements Provider {
         let data
         try {
           data = JSON.parse(message)
-          const text = data.choices[0].text
-          if (text === '<|im_end|>' || text === '<|im_sep|>') {
+          const text = data.choices[0].delta.content
+          if (text === undefined || text === '<|im_end|>' || text === '<|im_sep|>') {
             return
           }
           result += text
