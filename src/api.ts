21cd82f4f082ddae7b0db147f9d0f032ffa6e69b
Nikita Mitasov <32384814+ch4og@users.noreply.github.com>
2023-03-05 21:48:01 +0300

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -24,9 +24,8 @@ export async function fetchExtensionConfigs(): Promise<{
   chatgpt_webapp_model_name: string
   openai_model_names: string[]
 }> {
-  return fetch(`${API_HOST}/api/config`, {
-    headers: {
-      'x-version': getExtensionVersion(),
-    },
-  }).then((r) => r.json())
+ return {
+    chatgpt_webapp_model_name: 'text-davinci-002-render',
+    openai_model_names: ['gpt-3.5-turbo'],
+  }
 }
